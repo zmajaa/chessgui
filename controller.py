dd7dfb90ab4d4a0fde78e065a5778a9f7872bf41
Maja <34302901+zmajaa@users.noreply.github.com>
2018-01-24 13:28:23 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -9,9 +9,6 @@ class Controller():
     def init_model(self):
         self.model=model.Model()
 
-    def get_all_peices_on_chess_board(self):
-        return self.model.items()
-
     def reset_game_data(self):
         self.model.reset_game_data()
 
@@ -20,11 +17,27 @@ class Controller():
 
     def get_numeric_notation(self, position):
         return piece.get_numeric_notation(position)
-    #added methods
+
     def get_alphanumeric_position(self, rowcol):
-        if self.is_on_board(rowcol):
-            row, col = rowcol
-            return "{}{}".format(X_AXIS_LABELS[col],Y_AXIS_LABELS[row])
-    def is_on_board(self, rowcol):
-        row, col = rowcol
-        return 0 <= row <= 7 and 0 <= col <= 7
+        return self.model.get_alphanumeric_position(rowcol)
+        # if self.is_on_board(rowcol):
+        #     row, col = rowcol
+        #     return "{}{}".format(X_AXIS_LABELS[col],Y_AXIS_LABELS[row])
+        # def is_on_board(self, rowcol):
+        #     row, col = rowcol
+        #     return 0 <= row <= 7 and 0 <= col <= 7
+
+    def get_piece_at(self, position_of_click):
+        return self.model.get_piece_at(position_of_click)
+
+    def pre_move_validation(self, start_pos, end_pos):
+        return self.model.pre_move_validation(start_pos, end_pos)
+
+    def get_all_peices_on_chess_board(self):
+        return self.model.items()
+
+    def player_turn(self):
+        return self.model.player_turn
+
+    def moves_available(self, position):
+        return self.model.moves_available(position)
